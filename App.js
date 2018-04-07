743621c98eba3fb8fdce3da0526a90deaaae604d
tongyu110 <pengchengxue110@163.com>
2018-04-07 23:58:29 +0800

    init

---
+++ App.js
@@ -0,0 +1,22 @@
+import React , {Component} from 'react';
+import {Text,View} from 'react-native';
+class HelloWord extends Component {
+  render() {
+    return (
+      <Text> Hello {this.props.name} </Text>
+    );
+  }
+}
+
+export default class TellMe extends Component  {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <HelloWord name="Abu" />
+        <HelloWord name="Zhi" />
+        <HelloWord name="Fei" />
+        <HelloWord name="Jing" />
+      </View>
+    );
+  }
+}
